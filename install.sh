fde1231accc0ba92ca416f989ed4f6975150d7c9
Atsushi Nagase <a@ngs.io>
2020-05-04 05:04:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
+DIR=$(cd $(dirname $0) && pwd)
+for F in $(find $DIR -type f -name '.*'); do
+    FN=$(basename $F)
+    rm -f "${HOME}/${FN}"
+    ln -s "${DIR}/${FN}" "${HOME}/${FN}"
+done
 
+for D in $(find $DIR -type d -name '.*' \( ! -name .git \)); do
+    DN=$(basename $D)
+    for F in $(find $D -type f); do
+        FN=$(basename $F)
+        mkdir -p "${HOME}/${DN}"
+        rm -f "${HOME}/${DN}/${FN}"
+        ln -s $F "${HOME}/${DN}/${FN}"
+    done
+done
